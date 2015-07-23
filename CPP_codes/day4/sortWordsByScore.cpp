3330ce921c5be3a2fc811090cd805a48b6dac31a
Anubhav Chaturvedi <mailforanubhav@gmail.com>
2015-07-23 10:23:00 +0530

    changes in formatting and variable names

--- CPP_codes/day4/sortWordsByScore.cpp
+++ CPP_codes/day4/sortWordsByScore.cpp
@@ -4,16 +4,16 @@
 #include <fstream>
 #include <sstream>
 using namespace std;
-#define SPACE_DELIMITER " "
+
+const string DELIMITER = " ";
 
 const int ALPHABET_SCORE[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20, 21,22,23,24,25,26};
 
-bool openFile(string filename, ifstream& file)
-{
+bool openFile(string filename, ifstream& file) {
     file.open(filename.c_str());
     bool fileExists = file.is_open();
     if ( !fileExists ) {
-        cout << "unable to open file" ;
+        cout << "unable to open file" << endl ;
     }
     return fileExists;
 }
@@ -30,64 +30,50 @@ std::pair<int,string> computeScore(string str) {
 }
 
 void insertInMap(std::pair<int, string> currentPair, map<int, string> &valueWord) {
-  
-  map<int, string>::iterator it = valueWord.begin();
-	
-  if( valueWord.count(currentPair.first) ) {
-    it = valueWord.find( currentPair.first );
-    it -> second = it->second + SPACE_DELIMITER + currentPair.second;
-  }
-  else {
-    valueWord.insert ( std::pair<int, string>(currentPair.first, currentPair.second));
-  }
+
+    map<int, string>::iterator it = valueWord.begin();
+
+    if( valueWord.count(currentPair.first) ) {
+        it = valueWord.find( currentPair.first );
+        it -> second = it->second + DELIMITER + currentPair.second;
+    } else {
+        valueWord.insert ( std::pair<int, string>(currentPair.first, currentPair.second));
+    }
 }
 
 void writeMapToFile(map<int, string> wordValue) {
-	int count = 0;
-  map<int, string>::iterator it = wordValue.begin();
-  ofstream outFile ("wordValue.txt");
-  if (outFile.is_open()){
-    for (it=wordValue.begin(); it!=wordValue.end(); ++it) {
-    		count ++;
-    		outFile << it->second << '\n';
+    map<int, string>::iterator it = wordValue.begin();
+    ofstream outFile ("wordValue.txt");
+    if (outFile.is_open()) {
+        for (it=wordValue.end(); it!=wordValue.begin(); --it) {
+            outFile << it->first << "\t\t" << it->second << endl;
+        }
+        outFile.close();
+        cout<< "score and words written to file" << endl;
+    } else {
+        cout << "Unable to open file" << endl;
     }
-    outFile.close();
-    cout<< count <<" words written to file";
-  }
-  else{
-    cout << "Unable to open file";
-  } 	
 }
 
-void pushFileContentToMap(ifstream &file, map<int, string> &valueWord)
-{
+void pushFileContentToMap(ifstream &file, map<int, string> &valueWord) {
     string word;
-    while(getline(file,word))
-    {
+    while(getline(file,word)) {
+        cout << "'" << word << "'" <<endl;
         insertInMap(computeScore(word), valueWord);
     }
     writeMapToFile(valueWord);
 }
 
-int main(int argc, char *argv[])
-{
-	map<int, string> valueWord;
-    if (argc < 2)
-    {
-        cout << "enter the file name";
-        return 0;
-    }
+int main(int argc, char *argv[]) {
+    map<int, string> valueWord;
 
     ifstream file;
-
-    if (openFile(argv[1], file))
-    {
-    	cout<<"file opened";
+    string filename = "wordList.txt";
+    if (openFile(filename.c_str(), file)) {
+        cout << "file opened" << endl;
         pushFileContentToMap(file, valueWord);
-    }
-    else
-    {
-        cout << "file not opened";
+    } else {
+        cout << "file not opened" << endl;
     }
 }
 
