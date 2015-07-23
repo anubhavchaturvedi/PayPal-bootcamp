d362c870fca7b42a26599f9ae44d0b5b58b69107
garvitjain <garvitjain314@gmail.com>
2015-07-23 15:19:50 +0530

    adding more functions

--- CPP_codes/day4/ScrabbleWordHelper.cpp
+++ CPP_codes/day4/ScrabbleWordHelper.cpp
@@ -1,57 +1,104 @@
 #include <iostream>
+#include <map>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
 #include <set>
-#include <math.h>
-
 using namespace std;
 
-class ScrabbleWordHelper
-{
-private:
-    set <string> rackSubStringSet;
-public:
-    string rack;
+class scrabbleWordHelper {
+	private:
+	std::map<string,vector<string> > sowpods;
+	set <string> rackSubset;
+	public:
+		string rack;
+		
+		ScrabbleWordHelper(string rack)	{
+    		this->rack = rack;
+		}
+		
+		void findRackSubset(void)
+		{
+    		int set_size = rack.length();
+    		unsigned int pow_set_size = pow(2, set_size);
+    		int counter, j;
+    		string subset;
 
-    void findRackSubstring();
-    ScrabbleWordHelper(string rack);
+		    for(counter = 0; counter < pow_set_size; counter++) {
+      			subset = "";
+      			for(j = 0; j < set_size; j++) {
+          			if(counter & (1<<j))
+            			subset += rack[j];
+       			}
+        		rackSubset.insert(subset);
+    		}
 
+    		for(set<string>::iterator it=rackSubset.begin(); it!=rackSubset.end(); ++it) {
+        		cout << *it << endl;
+    		}
+		}
+		
+		std::pair<string, string> getSortedString(string word) {
+			std::string sortedWord = word;
+			std::sort(sortedWord.begin(), sortedWord.end());
+			return std::pair<string, string> (sortedWord, word);
+		}
+		
+		void insertInMap(std::pair<string, string> entry) {
+			map<string, vector<string> >::iterator it = sowpods.begin();
+	
+  			if( sowpods.count(entry.first) ) {
+    			it = sowpods.find( entry.first );
+    			(it->second).push_back(entry.second);
+    			cout << "\nKey Exists : " << entry.first << " adding " << entry.second << " to vector.";
+  			}
+  			
+  			else {
+  				vector<string> newVector;
+  				newVector.push_back(entry.second);
+    			sowpods.insert ( std::pair<string, vector<string> >(entry.first, newVector) );
+    			cout << "\nNew Key : " << entry.first;
+  			}
+		}
+		
+		void generateSowpodsMap(ifstream &file) {
+			string word;
+    		while(getline(file, word)) {
+        		insertInMap(getSortedString(word));
+    		}
+		}
 };
 
-ScrabbleWordHelper::ScrabbleWordHelper(string rack)
-{
-    this->rack = rack;
+bool openFile(string filename, ifstream& file) {
+    file.open(filename.c_str());
+    bool fileExists = file.is_open();
+    if ( !fileExists ) {
+        cout << "unable to open file" ;
+    }
+    return fileExists;
 }
 
-void  ScrabbleWordHelper::findRackSubstring(void)
-{
-
-    int set_size = rack.length();
-    unsigned int pow_set_size = pow(2, set_size);
-    int counter, j;
-    string subset;
-
-
-    for(counter = 0; counter < pow_set_size; counter++)
-    {
-      subset = "";
-      for(j = 0; j < set_size; j++)
-       {
-          if(counter & (1<<j))
-            subset += rack[j];
-       }
-        rackSubStringSet.insert(subset);
+int main(int argc, char* argv[]) {
+	scrabbleWordHelper scrabble;
+	
+	if (argc < 2) {
+        cout << "enter the file name";
+        return 0;
     }
 
-    for(set<string>::iterator it=rackSubStringSet.begin(); it!=rackSubStringSet.end(); ++it)
-    {
-        cout << *it << endl;
+    ifstream file;
+
+    if (openFile(argv[1], file)) {
+    	cout<<"file opened";
+        scrabble.generateSowpodsMap(file);
     }
-}
-
-int main(int argc, char *argv[])
-{
-   ScrabbleWordHelper scrabbleWordHelper("abcdef");
-   scrabbleWordHelper.findRackSubstring();
-
-
-
+    
+    else {
+        cout << "file not opened";
+    }
+	
+	return 0;
 }
