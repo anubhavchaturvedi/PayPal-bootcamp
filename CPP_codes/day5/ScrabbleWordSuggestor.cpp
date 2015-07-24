e787a4bc5fd8659e7901dbf2f8fdb4d28eceb32e
Anubhav Chaturvedi <mailforanubhav@gmail.com>
2015-07-24 03:04:03 -0700

    build fail issue fixed

--- CPP_codes/day5/ScrabbleWordSuggestor.cpp
+++ CPP_codes/day5/ScrabbleWordSuggestor.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <cmath>
 #include <set>
+#include <regex>
 
 using namespace std;
 
@@ -16,7 +17,8 @@ const int NUMBER_OF_OUTPUT = 5;
 
 const int ALPHABET_SCORE[] = {1,3,3,2, 1,4,2,4, 1,8,5,1,3, 1,1,3,10, 1,1,1,1, 4,4,8,4, 10};
 
-class ScrabbleWordSuggestor {
+class ScrabbleWordSuggestor
+{
 
 private:
     map< string, vector<string> > sowpods;
@@ -25,22 +27,26 @@ private:
     string RACK_STRING;
     string regExpression;
 private:
-    int getCharScore(char ch) {
+    int getCharScore(char ch)
+    {
         return ALPHABET_SCORE[ ch - 'a' ];
     }
 
-    void generatePowerSetOfRack( string rack, set<string>& power_set ) {
+    void generatePowerSetOfRack( string rack, set<string>& power_set )
+    {
         int set_size = rack.length();
         unsigned int pow_set_size = pow(2, set_size);
         int counter, j;
         string subset;
         int multiplicationFactor;
 
-        for(counter = 1; counter < pow_set_size; counter++) {
+        for(counter = 1; counter < pow_set_size; counter++)
+        {
             subset = "";
 
 
-            for(j = 0; j < set_size; j++) {
+            for(j = 0; j < set_size; j++)
+            {
                 multiplicationFactor = pow (2, j);
                 if(counter & (1 * multiplicationFactor))
                     subset += rack[j];
@@ -49,89 +55,113 @@ private:
         }
     }
 
-    void generateBlankReplacedPowerSet( set<string>& power_set, set< pair<string,int> >& power_set_score ) {
-        for ( string power_set_element : power_set ) {
+    void generateBlankReplacedPowerSet( set<string>& power_set, set< pair<string,int> >& power_set_score )
+    {
+        for ( string power_set_element : power_set )
+        {
             replaceBlankTile( power_set_element, power_set_score );
         }
     }
 
-    void replaceBlankTile( string rack, set< std::pair<string,int> >& set_of_racks ) {
+    void replaceBlankTile( string rack, set< std::pair<string,int> >& set_of_racks )
+    {
         replaceBlankTile( rack, 0, set_of_racks );
     }
 
-    void replaceBlankTile( string rack, int scoreDeductionValue, set< std::pair<string,int> >& set_of_racks ) {
+    void replaceBlankTile( string rack, int scoreDeductionValue, set< std::pair<string,int> >& set_of_racks )
+    {
         int rackBlankTileIndex = rack.find(EMPTY_TILE);
-        if ( rackBlankTileIndex >= 0 ) {
-            for ( char ch = 'a'; ch <='z'; ch++ ) {
+        if ( rackBlankTileIndex >= 0 )
+        {
+            for ( char ch = 'a'; ch <='z'; ch++ )
+            {
                 rack[rackBlankTileIndex] = ch;
                 int newScoreDectionValue = scoreDeductionValue + getCharScore(ch);
                 replaceBlankTile( rack, newScoreDectionValue, set_of_racks);
             }
-        } else {
+        }
+        else
+        {
             set_of_racks.insert( pair<string,int> ( getSortedString(rack), scoreDeductionValue) );
         }
     }
 
-    string getSortedString(string word) {
+    string getSortedString(string word)
+    {
         std::string sortedWord = word;
         std::sort(sortedWord.begin(), sortedWord.end());
         return sortedWord;
     }
 
-    void insertInMap(string key, string value) {
+    void insertInMap(string key, string value)
+    {
         map<string, vector<string> >::iterator it = sowpods.begin();
 
-        if( sowpods.count(key) ) {
+        if( sowpods.count(key) )
+        {
             it = sowpods.find( key );
             (it->second).push_back(value);
         }
 
-        else {
+        else
+        {
             vector<string> newVector;
             newVector.push_back(value);
             sowpods.insert ( std::pair<string, vector<string> >( key, newVector) );
         }
     }
 
-    void generateSowpodsMap(ifstream &file) {
+    void generateSowpodsMap(ifstream &file)
+    {
         string word;
-        while(getline(file, word)) {
-                insertInMap(getSortedString(word), word);
+        while(getline(file, word))
+        {
+            insertInMap(getSortedString(word), word);
         }
     }
 
-    vector<string> getAnagramListFromSowpodsMap(string key) {
+    vector<string> getAnagramListFromSowpodsMap(string key)
+    {
         vector<string> anagramList;
-        if( sowpods.find(key) != sowpods.end() ) {
+        if( sowpods.find(key) != sowpods.end() )
+        {
             anagramList = sowpods.find( key )->second;
         }
         return anagramList;
     }
 
-    int computeScore(string str, int scoreCompensation) {
+    int computeScore(string str, int scoreCompensation)
+    {
         int score = 0;
-        for ( int i = 0; i < str.length(); i++ ) {
+        for ( int i = 0; i < str.length(); i++ )
+        {
             char ch = str[i];
-            if ( 'a' <= ch && ch <= 'z' ) {
+            if ( 'a' <= ch && ch <= 'z' )
+            {
                 score += ALPHABET_SCORE[ ch - 'a' ];
             }
         }
         return score - scoreCompensation;;
     }
 
-    void insertInScoredList(int score, string word) {
+    void insertInScoredList(int score, string word)
+    {
         map<int, vector<string> >::iterator it = scored_list.begin();
-        if ( scored_list.find(score) != scored_list.end() ) {
+        if ( scored_list.find(score) != scored_list.end() )
+        {
             it = scored_list.find( score );
             (it->second).push_back(word);
-        } else {
+        }
+        else
+        {
             vector<string> newVector;
             newVector.push_back(word);
             scored_list.insert ( std::pair<int, vector<string> >(score, newVector) );
         }
     }
 
-    void generateScoredList(string rack) {
+    void generateScoredList(string rack)
+    {
         this->RACK_STRING = rack;
         set<string> powerSet;
 
@@ -141,65 +171,84 @@ private:
         generatePowerSetOfRack(rack, powerSet);
         generateBlankReplacedPowerSet(powerSet, POWERSET_RACKS);
 
-        for ( pair<string,int> p : POWERSET_RACKS ) {
+        for ( pair<string,int> p : POWERSET_RACKS )
+        {
             int score = computeScore( p.first, NO_SCORE_COMPENSATION );
-            for ( string anagram : getAnagramListFromSowpodsMap( p.first ) ) {
+            for ( string anagram : getAnagramListFromSowpodsMap( p.first ) )
+            {
                 int scoreCompensation = p.second;
                 insertInScoredList( score - scoreCompensation, anagram);
             }
         }
     }
 
-    void addConstraintToRack(string constraint) {
-    	for(int index = 0; index < constraint.length(); index++) {
-    		char letter = constraint.at(index);
-    		if(letter != '*') {
-    			this->RACK_STRING.push_back(letter);
-		}
-	}
+    void addConstraintToRack(string constraint)
+    {
+        for(int index = 0; index < constraint.length(); index++)
+        {
+            char letter = constraint.at(index);
+            if(letter != '*')
+            {
+                this->RACK_STRING.push_back(letter);
+            }
+        }
     }
 
-    void removeConstraintFromRack(string constraint) {
-	for(int index = 0; index < constraint.length(); index++) {
-		char letter = constraint.at(index);
-    		if(letter != '*') {
-    			this->RACK_STRING.erase(this->RACK_STRING.find(letter), 1);
-		}
-	}
+    void removeConstraintFromRack(string constraint)
+    {
+        for(int index = 0; index < constraint.length(); index++)
+        {
+            char letter = constraint.at(index);
+            if(letter != '*')
+            {
+                this->RACK_STRING.erase(this->RACK_STRING.find(letter), 1);
+            }
+        }
     }
 
     string getRegularExpression(string constraint)
     {
-    	string regularExpression="";
-    	for(int i=0;i<constraint.length();i++)
-    	{
-    	if(constraint[i]=='*')
-    		regularExpression+="[a-z]{0,1}";
-    	else
-    		regularExpression+=constraint[i];
-		}
-    	return regularExpression;
-	}
-	
-	bool matchesRegularExpression(string word,string regularExpression)
-	{
-	return regex_match (word, regex(regularExpression));
-	}
+        string regularExpression="";
+        for(int i=0; i<constraint.length(); i++)
+        {
+            if(constraint[i]=='*')
+                regularExpression+="[a-z]{0,1}";
+            else
+                regularExpression+=constraint[i];
+        }
+        return regularExpression;
+    }
+
+    bool matchesRegularExpression(string word,string regularExpression)
+    {
+        return regex_match (word, regex(regularExpression));
+    }
+
+    bool constraintPassed(string word)
+    {
+        if (regex_match (word, regex(regExpression) ))
+            return true;
+        return false;
+    }
 
 
 public:
-    ScrabbleWordSuggestor(ifstream& sowpodsFile) {
+    ScrabbleWordSuggestor(ifstream& sowpodsFile)
+    {
         generateSowpodsMap(sowpodsFile);
     }
 
-    void suggestWords(string rack){
+    void suggestWords(string rack)
+    {
         generateScoredList(rack);
         int outputCount = 0;
 
-        for ( map<int, vector<string> >::reverse_iterator r = scored_list.rbegin(); r != scored_list.rend() && outputCount < NUMBER_OF_OUTPUT; ++r, outputCount++ ) {
+        for ( map<int, vector<string> >::reverse_iterator r = scored_list.rbegin(); r != scored_list.rend() && outputCount < NUMBER_OF_OUTPUT; ++r, outputCount++ )
+        {
 
             cout << r->first << "\t\t" ;
-            for ( string s : r->second ) {
+            for ( string s : r->second )
+            {
                 cout << s << " " ;
             }
             cout << endl;
@@ -207,28 +256,23 @@ public:
     }
 };
 
-bool constraintPassed(string word)
+int main(int argc, char* argv[])
 {
-  if (regex_match (word, regex(regExpression) ))
-    return true;
-return false;
-}
+    ifstream sowpodsFile;
+    string FILENAME = "sowpods.txt";
 
-int main(int argc, char* argv[]) {
-   ifstream sowpodsFile;
-   string FILENAME = "sowpods.txt";
-
-   try
-   {
-       sowpodsFile.open(FILENAME.c_str());
-       ScrabbleWordSuggestor scrabble(sowpodsFile);
-       scrabble.suggestWords("apple*d");
-       cout << "======================================================================================================================" << endl;
-       cout << "======================================================================================================================" << endl;
-       scrabble.suggestWords("abcdef");
-   }
-    catch (std::ifstream::failure e) {
+    try
+    {
+        sowpodsFile.open(FILENAME.c_str());
+        ScrabbleWordSuggestor scrabble(sowpodsFile);
+        scrabble.suggestWords("apple*d");
+        cout << "======================================================================================================================" << endl;
+        cout << "======================================================================================================================" << endl;
+        scrabble.suggestWords("abcdef");
+    }
+    catch (std::ifstream::failure e)
+    {
         std::cerr << "Exception opening/reading/closing file\n";
- 	}
-   return 0;
+    }
+    return 0;
 }
