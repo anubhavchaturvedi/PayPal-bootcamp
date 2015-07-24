567ea0591852663bc8374f67e063ae8c308af6eb
Rubeenaks <rubeenaks@gmail.com>
2015-07-25 00:36:22 +0530

    added a few test cases

--- CPP_codes/day5/TestScrabbleWordSuggestor.cpp
+++ CPP_codes/day5/TestScrabbleWordSuggestor.cpp
@@ -4,22 +4,47 @@
 namespace
 {
 
-string FILENAME = "sowpods.txt";
-ScrabbleWordSuggestor scrabble(FILENAME);
-TEST(valid_score_check)
-{
-    const int result=scrabble.computeScore("ab",0);
-    CHECK_EQUAL(4,result);
-}
-
-TEST(Testcase1)
-{
-    const int result=scrabble.computeScore("cddddd",0);
-    CHECK_EQUAL(2,result);
+    ScrabbleWordSuggestor obj("sowpods.txt");
+    TEST(valid_score_check)
+    {
+        const int result=obj.computeScore("ab",0);
+        CHECK_EQUAL(4,result);
 
 
-}
+    }
+    TEST(inalid_score_check)
+    {
+        const int result=obj.computeScore("cddddd",0);
+        CHECK_EQUAL(2,result);
 
+
+    }
+
+ TEST(check_string_sorting)
+    {
+        const string result=obj.getSortedString("dcba");
+        CHECK_EQUAL("abcd",result);
+
+
+    }
+
+
+ TEST(regex_generator_test)
+    {
+        const string result=obj.generateRegExp("*b*");
+        CHECK_EQUAL(".{0,1}b.{0,1}",result);
+
+
+    }
+
+    TEST(regex_comparing_test)
+    {
+        obj.rackRegExp = ".{0,1}a.{0,1}";
+        const bool result=obj.isMatchesRegularExpression("cat");
+        CHECK_EQUAL(true, result);
+
+
+    }
 
 
 }
