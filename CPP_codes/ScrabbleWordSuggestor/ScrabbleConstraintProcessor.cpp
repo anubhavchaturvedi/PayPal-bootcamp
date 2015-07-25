7fd2ec0e4f852272dc6525d118400751decc5128
VidyashreeBL <vidu_friend@hotmail.com>
2015-07-25 11:39:57 +0530

    Added isContraintPassed functionality
    isConstaintPassed() takes a word and checks if the word adheres to the constraint.

--- CPP_codes/ScrabbleWordSuggestor/ScrabbleConstraintProcessor.cpp
+++ CPP_codes/ScrabbleWordSuggestor/ScrabbleConstraintProcessor.cpp
@@ -44,4 +44,15 @@ string ScrabbleConstraintProcessor::generateConstraintRegExp(string constraint)
         return (startExp + midExp + endExp);
 }
 
+bool isConstraintPassed(string word){
+    try {
+            return std::regex_match(word, std::regex(rackRegExp));
+        }
+    catch (const std::regex_error& e) {
+        std::cout << "regex_error caught: " << e.what() << '\n';
+        if (e.code() == std::regex_constants::error_brack) {
+            std::cout << "The code was error_back\n";
+        }
+    }
+}
 
