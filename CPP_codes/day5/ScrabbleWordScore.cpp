c4d525ae17664b0704605fc0c34a15d0b19fe7b5
manish <manishengi25@gmail.com>
2015-07-25 11:34:37 +0530

    Added ComputeScore Function

--- CPP_codes/day5/ScrabbleWordScore.cpp
+++ CPP_codes/day5/ScrabbleWordScore.cpp
@@ -1,3 +1,35 @@
 #include "ScrabbleWordScore.h"
 
+ScrabbleWordScore::ScrabbleWordScore()
+{
+    score=0;
+}
 
+int ScrabbleWordScore::getLetterScore(char letter)
+{
+    return ALPHABET_SCORE[ letter - 'a' ];
+}
+
+int ScrabbleWordScore::computeWordScore(string word)
+{
+
+        for ( int i = 0; i < word.length(); i++ )
+        {
+            char ch = word[i];
+            if ( 'a' <= ch && ch <= 'z' )
+            {
+                score += getLetterScore(ch);
+            }
+        }
+
+        return score;
+}
+
+int ScrabbleWordScore::computeWordScore(string word, string lettersToIgnore)
+{
+    int word_score = computeWordScore(word);
+    int lettersToIgnore_score = computeWordScore(lettersToIgnore);
+
+    return word_score-lettersToIgnore_score;
+
+}
--- CPP_codes/day5/ScrabbleWordScore.h
+++ CPP_codes/day5/ScrabbleWordScore.h
@@ -1,9 +1,10 @@
 #include <iostream>
+using namespace std;
 
 class ScrabbleWordScore
 {
     private:
-            int ALPHABET_SCORE[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+           const int ALPHABET_SCORE[26] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
             int score;
     public:
 
