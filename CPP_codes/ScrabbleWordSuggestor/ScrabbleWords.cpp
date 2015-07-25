e7be6f57ad3ffe6e8a14a28a52e53aef1c9ca6a3
Asha Jyothi <ashajyothi.pryr@gmail.com>
2015-07-25 11:32:06 +0530

    function to sort the string

--- CPP_codes/ScrabbleWordSuggestor/ScrabbleWords.cpp
+++ CPP_codes/ScrabbleWordSuggestor/ScrabbleWords.cpp
@@ -4,3 +4,7 @@ ScrabbleWords::ScrabbleWords(string filename) {
   ifstream sowpodsFile;
   sowpodsFile.open(filename.c_str());
 }
+
+string getSortedString(string str){
+    std::sort(word.begin(), word.end());
+    }
