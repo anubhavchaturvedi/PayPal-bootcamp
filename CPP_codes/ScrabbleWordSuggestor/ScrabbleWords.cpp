21db5becce928b0774d7cecc30a2bdb6e6781cbc
FaizHalde <faiz_84123@yahoo.com>
2015-07-25 12:25:34 +0530

    Removed main function. Testing done

--- CPP_codes/ScrabbleWordSuggestor/ScrabbleWords.cpp
+++ CPP_codes/ScrabbleWordSuggestor/ScrabbleWords.cpp
@@ -11,40 +11,21 @@ ScrabbleWords::ScrabbleWords(string filename) {
 
 void ScrabbleWords::insertInSowpodsMap(string key, string value)
     {
-        map<string, vector<string> >::iterator it = sowpods.begin();
-
-        if( sowpods.count(key) )
-        {
-            it = sowpods.find( key );
-            (it->second).push_back(value);
-        }
-
-        else
-        {
-            vector<string> newVector;
-            newVector.push_back(value);
-            sowpods.insert ( std::pair<string, vector<string> >( key, newVector) );
-        }
+    	sowpods[key].push_back(value);
     }
     
     
-    string getSortedString(string str){
-        std::sort(word.begin(), word.end());
+    string ScrabbleWords::getSortedString(string word){
+    	string sortedWord = word;
+        std::sort(sortedWord.begin(), sortedWord.end());
+        return sortedWord;
     }
 
 vector<string> ScrabbleWords::getAnagrams(string word) {
 	string sortedWord = getSortedString(word);
 	vector<string> anagrams;
 	if(sowpods.find(sortedWord) != sowpods.end()) {
-		vector<string> anagrams = sowpods[sortedWord];
+		anagrams = sowpods[sortedWord];
 	}
 	return anagrams;
-}
-
-int main() {
-	ScrabbleWords scr("sowpods.txt");
-	vector<string> a = scr.getAnagrams("friends");
-	for(string s : a) {
-		cout << s << endl;
-	}
 }
